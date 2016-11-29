883643a33ae21f7c4fee432c05c96f4907df66c5
Reain <einar.acuna@gmail.com>
2016-11-29 18:32:04 -0500

    The changes I want to push to my branch.

--- GitHubTutorial/Controllers/MultipleUsersController.cs
+++ GitHubTutorial/Controllers/MultipleUsersController.cs
@@ -15,8 +15,15 @@ namespace GitHubTutorial.Controllers
         }
 
         public ActionResult WhatTheTeamIsWorkingOn() {
-            var x = 1;
-            var y = 50;
+            //var x = 1;
+            //var y = 50;
+
+            //For this example, the above values are in Master.
+            //I'm changing it to below plus adding another variable.
+
+            var x = 13;
+            var y = 5213;
+            var BranchAdded = 123;
             return View();
         }
     }
