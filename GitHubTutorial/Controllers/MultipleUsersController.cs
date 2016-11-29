816aeec046628d0c4a7a3ea2e02e979a5b71f428
Reain <einar.acuna@gmail.com>
2016-11-29 18:21:30 -0500

    Asdf

---
+++ GitHubTutorial/Controllers/MultipleUsersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitHubTutorial.Controllers
+{
+    public class MultipleUsersController : Controller
+    {
+        // GET: MultipleUsers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult WhatTheTeamIsWorkingOn() {
+            var x = 1;
+            var y = 50;
+            return View();
+        }
+    }
+}
