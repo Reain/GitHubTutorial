971c417d928d7ea43d957ff36af98dddf043a359
Reain <einar.acuna@gmail.com>
2016-11-29 18:09:55 -0500

    Stuff

---
+++ GitHubTutorial/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitHubTutorial.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public class ThisIsSomethingThatIsEdited {
+            //Here for our example is a class that is going to be edited. This is master.
+        }
+
+        public class StuffThatIsAddedFromOtherUsers {
+
+        }
+
+    }
+}
