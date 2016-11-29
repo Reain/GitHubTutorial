f5ca9bca3908d7e704a4e4525f190327560390b0
Reain <einar.acuna@gmail.com>
2016-11-29 18:33:16 -0500

    adsf

--- GitHubTutorial/Controllers/MultipleUsersController.cs
+++ GitHubTutorial/Controllers/MultipleUsersController.cs
@@ -15,8 +15,13 @@ namespace GitHubTutorial.Controllers
         }
 
         public ActionResult WhatTheTeamIsWorkingOn() {
-            var x = 1;
-            var y = 50;
+
+            //In master, someone else had made changes different from the ones you made.
+            //var x = 1;
+            //var y = 50;
+            var x =981;
+            var y = 9920;
+            var a = 12445;
             return View();
         }
     }
